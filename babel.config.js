cf38d6721fd73a8ec0a85452e56e33d5c57b9a5a
Adrien Boucaud <adrien.bk@free.fr>
2020-12-29 00:57:09 +0100

    Add aliases

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/cli-plugin-babel/preset"],
+
+  plugins: [
+    [
+      "module-resolver",
+      {
+        'alias': {
+          "@components": "./src/components",
+          "@helpers": "./src/helpers",
+          "@store": "./src/store",
+        },
+      },
+    ],
+  ],
+};
