9bcb2bf788908fe20a9b98fc64b8d7fbe760a4f1
Adrien Boucaud <adrien.bk@free.fr>
2020-12-29 01:48:22 +0100

    SFC everywhere

--- src/components/core/App/index.vue
+++ src/components/core/App/index.vue
@@ -1,3 +1,38 @@
-<template src="./template.html"/>
-<style />
-<script src="./script.js" />
+<template>
+  <v-app>
+    <!-- <v-navigation-drawer app>
+  </v-navigation-drawer> -->
+
+    <v-app-bar app>
+      <!-- -->
+      Youtube shuffler
+    </v-app-bar>
+
+    <!-- Provides the application the proper gutter -->
+    <v-main>
+      <v-container fluid>
+        <URLInput />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import URLInput from "@components/selection/URLInput";
+
+export default {
+  name: "YoutubeShuffle",
+  components: {
+    URLInput,
+  },
+  data: () => ({}),
+  mounted: () => {},
+};
+</script>
+
+<style>
+</style>
