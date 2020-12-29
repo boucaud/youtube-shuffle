41531f617f92d23584d4d00e86679686c414fb96
Adrien Boucaud <adrien.bk@free.fr>
2020-12-29 01:22:59 +0100

    Add skeleton for list validation

--- src/components/selection/URLInput/index.vue
+++ src/components/selection/URLInput/index.vue
@@ -4,17 +4,29 @@
     <v-card-text>
       Each line should have one playlist URL, or playlist id
       <v-textarea v-model="rawURLList" />
-      <v-btn>Shuffle all videos in these playlists</v-btn>
+      <v-btn @click="onValidated">Shuffle all videos in these playlists</v-btn>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import { parseInputListToPlaylistIdArray } from "@helpers/inputList";
+
 export default {
   name: "URLInput",
   data: () => ({
     rawURLList: "",
   }),
+  methods: {
+    onValidated() {
+      const list = parseInputListToPlaylistIdArray(this.rawURLList);
+      if (!list || list.length === 0) {
+        // TODO: show error message
+        return;
+      }
+      console.log(list);
+    },
+  },
 };
 </script>
 
--- src/helpers/inputList.js
+++ src/helpers/inputList.js
@@ -1,3 +1,14 @@
-function inputListToPlaylistIdArray(listAsText) {}
+export function parseInputListToPlaylistIdArray(listAsText) {
+  if (listAsText.length === 0) {
+    return null;
+  }
+  const idArray = listAsText.split("\n").map((candidate) => {
+    return candidate;
+    // TODO: validate id
+    // TODO: check for existence ?? is it possible without youtube api ?
+    // TODO: also parse url
+  });
+  return idArray;
+}
 
-export default { inputListToPlaylistIdArray };
+export default { parseInputListToPlaylistIdArray };
