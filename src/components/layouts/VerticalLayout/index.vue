e6df36fb4413bc6ea98947bd276bc1be17533e07
Adrien Boucaud <adrien.bk@free.fr>
2020-12-31 00:48:55 +0100

    Disable vertical overflow

--- src/components/layouts/VerticalLayout/index.vue
+++ src/components/layouts/VerticalLayout/index.vue
@@ -1,5 +1,11 @@
+<style module>
+.container {
+  overflow-x: hidden;
+}
+</style>
+
 <template>
-  <div>
+  <div :class="$style.container">
     <slot name="player" />
     <v-divider />
     <slot name="controls" />
@@ -14,5 +20,3 @@ export default {
 };
 </script>
 
-<style>
-</style>
