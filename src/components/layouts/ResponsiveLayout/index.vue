a08dab24fefc096d8cfb1e2705cc9887b25f56f1
Adrien Boucaud <adrien.bk@free.fr>
2021-01-06 01:40:59 +0100

    Fix Youtube player on mobile

--- src/components/layouts/ResponsiveLayout/index.vue
+++ src/components/layouts/ResponsiveLayout/index.vue
@@ -1,12 +1,11 @@
 <template>
   <v-container fluid :class="$style.container">
     <v-row>
-      <v-col lg="6" md="8" sm="12">
+      <v-col lg="6" md="8" width="100%">
         <slot name="player" />
         <slot name="controls" />
       </v-col>
-      <v-spacer />
-      <v-col lg="6" md="4" sm="12">
+      <v-col lg="6" md="4">
         <slot name="playlist" />
       </v-col>
     </v-row>
