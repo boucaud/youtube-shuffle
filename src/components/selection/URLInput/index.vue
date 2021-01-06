111a79e6c5d68b5f7e3e2e481d2f8d06f0098702
Adrien Boucaud <adrien.bk@free.fr>
2021-01-06 02:04:19 +0100

    Wrap url input

--- src/components/selection/URLInput/index.vue
+++ src/components/selection/URLInput/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <v-card>
-    <v-card-title>Input the playlists you want to play</v-card-title>
-    <v-card-text>
-      Each line should have one playlist URL, or playlist id
-      <v-textarea
-        v-model="rawURLList"
-      />
-      <v-btn @click="onValidated">Shuffle all videos in these playlists</v-btn>
-    </v-card-text>
-  </v-card>
+  <v-container>
+    <v-card>
+      <v-card-title>Input the playlists you want to play</v-card-title>
+      <v-card-text>
+        Each line should have one playlist URL, or playlist id
+        <v-textarea v-model="rawURLList" />
+        <v-btn @click="onValidated"
+          >Shuffle all videos in these playlists</v-btn
+        >
+      </v-card-text>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
