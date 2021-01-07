d7433be3c35297a0037af201649cd412828b3659
Adrien Boucaud <adrien.bk@free.fr>
2021-01-07 23:39:45 +0100

    Cap default number of items shown, for performance

--- src/components/media/YoutubePlaylist/index.vue
+++ src/components/media/YoutubePlaylist/index.vue
@@ -1,16 +1,16 @@
 <template>
   <v-card :style="{ height: '100%' }">
     <v-card-title>
-      {{ `Current Mix (${activeVideoIndex + 1}/${videoArray.length})` }}
+      {{ `Current Mix (${activeVideoIndex + 1}/${numberOfItems})` }}
     </v-card-title>
     <v-card-text :style="{ height: '100%', maxHeight: '85vh' }">
       <v-list
         :style="{ overflowY: 'auto', maxHeight: '100%' }"
-        v-if="videoArray"
+        v-if="videoArray && numberOfItemsToDisplay"
       >
         <v-list-item-group mandatory v-model="activeVideoIndex">
           <v-list-item
-            v-for="(videoItem, i) in videoArray"
+            v-for="i in numberOfItemsToDisplay"
             :key="i"
             :style="{ minHeight: '7vh' }"
           >
@@ -19,14 +19,22 @@
               ><v-img
                 :aspect-ratio="16 / 9"
                 :lazy-src="require('./youtube.png')"
-                :src="`https://img.youtube.com/vi/${videoItem.id}/mqdefault.jpg`"
+                :src="`https://img.youtube.com/vi/${videoArray[i].id}/mqdefault.jpg`"
             /></v-list-item-avatar>
             <v-list-item-content>
-              <v-list-item-title>{{ videoItem.title }}</v-list-item-title>
+              <v-list-item-title>{{ videoArray[i].title }}</v-list-item-title>
               <v-list-item-subtitle>{{
-                videoItem.channel
+                videoArray[i].channel
               }}</v-list-item-subtitle>
             </v-list-item-content>
+          </v-list-item>
+          <v-list-item
+            v-if="numberOfItemsToDisplay < numberOfItems"
+            :key="numberOfItemsToDisplay + 1"
+          >
+            <v-list-item-title>
+              Load {{ increment }} more items...
+            </v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -43,18 +51,36 @@ export default {
   name: "YoutubePlaylist",
   data: () => ({
     activeVideoIndex: 0,
+    previousIndex: 0,
+    maxItems: 50,
+    increment: 50,
   }),
   computed: {
     ...mapGetters({
       currentVideoIndex: "getCurrentVideoIndex",
       videoArray: "getVideoArray",
     }),
+    numberOfItemsToDisplay() {
+      return Math.min(
+        this.videoArray ? this.videoArray.length : 0,
+        this.maxItems
+      );
+    },
+    numberOfItems() {
+      return this.videoArray ? this.videoArray.length : 0;
+    },
   },
   watch: {
     currentVideoIndex(index) {
       this.activeVideoIndex = index;
     },
     activeVideoIndex(index) {
+      if (index === this.numberOfItemsToDisplay) {
+        this.maxItems += this.increment;
+        this.activeVideoIndex = this.previousIndex;
+        return;
+      }
+      this.previousIndex = index;
       this.setVideoIndex(index);
     },
   },
