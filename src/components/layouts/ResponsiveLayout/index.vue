cdd0ffbb06320a57139503715bd3654a20173586
Adrien Boucaud <adrien.bk@free.fr>
2021-01-07 02:46:47 +0100

    Fix left-side layout

--- src/components/layouts/ResponsiveLayout/index.vue
+++ src/components/layouts/ResponsiveLayout/index.vue
@@ -2,8 +2,18 @@
   <v-container fluid :class="$style.container">
     <v-row>
       <v-col lg="6" md="8" width="100%">
-        <slot name="player" />
-        <slot name="controls" />
+        <v-container flex :style="{padding: 0}">
+          <v-row dense>
+            <v-col>
+              <slot name="player" />
+            </v-col>
+          </v-row>
+          <v-row dense>
+            <v-col>
+              <slot name="controls" my-8 />
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
       <v-col lg="6" md="4">
         <slot name="playlist" />
