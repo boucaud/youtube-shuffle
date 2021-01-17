e78b708d479b851ce27a4779284333c303d2ed17
Adrien Boucaud <adrien.bk@free.fr>
2021-01-17 22:21:03 +0100

    Add button to add new playlist

--- src/components/media/PlaylistInformation/index.vue
+++ src/components/media/PlaylistInformation/index.vue
@@ -3,6 +3,33 @@
     <v-card-title> Playing From </v-card-title>
     <v-card-text>
       <v-slide-group>
+        <v-slide-item>
+          <v-card elevation="4" class="ma-1">
+            <v-card-title>
+              <v-menu offset-y :close-on-content-click="false">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn icon large v-bind="attrs" v-on="on">
+                    <v-icon>{{ addIcon }}</v-icon>
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title> Add a new playlist </v-card-title>
+                  <v-card-text>
+                    <v-text-field
+                      label="Playlist ID or URL"
+                      v-model="newPlaylistInput"
+                    />
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn text @click="addPlaylist(newPlaylistInput)">
+                      Submit
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-menu>
+            </v-card-title>
+          </v-card>
+        </v-slide-item>
         <v-slide-item
           v-for="(id, i) in Object.keys(playlistInformation)"
           :key="i"
@@ -30,12 +57,14 @@
 
 <script>
 import { mapActions, mapGetters } from "vuex";
-import { mdiCloseCircleOutline } from "@mdi/js";
+import { mdiCloseCircleOutline, mdiPlusCircleOutline } from "@mdi/js";
 
 export default {
   name: "PlaylistInformation",
   data: () => ({
     removeIcon: mdiCloseCircleOutline,
+    addIcon: mdiPlusCircleOutline,
+    newPlaylistInput: "",
   }),
   computed: {
     ...mapGetters({
@@ -57,6 +86,9 @@ export default {
       this.redirectToIdArray({
         idArray: this.urlIdArray.filter((idInArray) => idInArray !== id),
       });
+    },
+    addPlaylist(id) {
+      this.redirectToIdArray({ idArray: [...this.urlIdArray, id] });
     },
   },
 };
